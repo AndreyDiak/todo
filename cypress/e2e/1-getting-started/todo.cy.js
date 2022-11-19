d7739e56d93c209de0389aae9b18cb6922d8863e
AndreyDiak <brodiaga_0880@mail.ru>
2022-11-20 00:28:22 +0300

    adding test on menu buttons

--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -11,41 +11,85 @@
 // please read our getting started guide:
 // https://on.cypress.io/introduction-to-cypress
 
-describe('example to-do app', () => {
+describe("example to-do app", () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000/')
-  })
+    cy.visit("http://localhost:3000/");
+  });
 
-  it('no messages at start', () => {
-    cy.get("[data-cy='todos-list']").contains('No tasks')
-  })
+  it("no messages at start", () => {
+    cy.get("[data-cy='todos-list']").contains("No tasks");
+  });
 
-  it('can add new todo items', () => {
+  it("can add new todo items", () => {
     cy.get("[data-cy='add-todo']")
       .click()
       .get("[data-cy='todos-list']")
-      .contains('No tasks');
+      .contains("No tasks");
 
     cy.get("[data-cy='todo-input']")
-      .type('Example todo')
+      .type("Example todo")
       .get("[data-cy='add-todo']")
       .click()
       .get("[data-cy='todos-list']")
-      .should('have.text', 'Example todo')
+      .should("have.text", "Example todo")
       .get("[data-cy='todo']")
-      .should('have.length', 1)
-  })
+      .should("have.length", 1);
+  });
 
-  it('checks items left counter', () => {
+  it("checks items left counter", () => {
     cy.get("[data-cy='todo-input']")
-      .type('Example todo')
+      .type("Example todo")
       .get("[data-cy='add-todo']")
       .click()
       .get("[data-cy='todo-left']")
-      .contains('1')
+      .contains("1")
       .get("[data-cy='todo']")
       .click()
       .get("[data-cy='todo-left']")
-      .contains('0')
-  })
-})
+      .contains("0");
+  });
+
+  it("checks menu items functionality", () => {
+    const text = "example todo";
+    const defaultText = "No tasks";
+
+    cy.get("[data-cy='todos-list']").as("todosList")
+      .get("[data-cy='todo-active']").as("todosActive")
+      .get("[data-cy='todo-completed']").as("todosCompleted");
+
+    cy.get("@todosList")
+      .contains(defaultText)
+      .get("[data-cy='todo-input']")
+      .type(text)
+      .get("[data-cy='add-todo']")
+      .click()
+      .get("@todosList")
+      .contains(text)
+      .get("@todosActive")
+      .click()
+      .get("@todosList")
+      .contains(text)
+      .get("@todosCompleted")
+      .click()
+      .get("@todosList")
+      .contains(defaultText)
+      .get("@todosActive")
+      .click()
+      .get("[data-cy='todo']")
+      .click()
+      .get("@todosCompleted")
+      .click()
+      .get("@todosList")
+      .contains(text)
+      .get("@todosActive")
+      .click()
+      .get("@todosList")
+      .contains(defaultText)
+      .get("[data-cy='todo-delete']")
+      .click()
+      .get("@todosCompleted")
+      .click()
+      .get("@todosList")
+      .contains(defaultText)
+  });
+});
